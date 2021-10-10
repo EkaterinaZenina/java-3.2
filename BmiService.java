8eae090b1f28399d864c4424c6fd69a2db75f5ef
EkaterinaZenina <90308326+EkaterinaZenina@users.noreply.github.com>
2021-10-10 09:57:25 +0300

    Update BmiService.java

--- BmiService.java
+++ BmiService.java
@@ -4,11 +4,7 @@ public class BmiService {
         double height = 0.0;
         bmi = (weight)/(height * height);
         System.out.println("Your BMI is " + bmi);
-        System.out.println("BMI VALUES");
-        System.out.println("Underweight: Under 18.5");
-        System.out.println("Normal: 18.5-24.9 ");
-        System.out.println("Overweight: 25-29.9");
-        System.out.println("Obese: 30 or over");
+
     }
 
 }
